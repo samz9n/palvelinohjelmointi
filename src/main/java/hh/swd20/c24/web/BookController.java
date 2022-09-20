53199fad6ecfef888f62bbdff1826adf48f67ab5
samz9n <samuel.langhoff@outlook.com>
2022-09-20 13:54:43 +0300

    tehtävä 3_3

--- src/main/java/hh/swd20/c24/web/BookController.java
+++ src/main/java/hh/swd20/c24/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.swd20.c24.domain.Book;
 import hh.swd20.c24.domain.BookRepo;
 
 @Controller
@@ -21,5 +24,23 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@GetMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@PostMapping(value = "/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:/booklist";
+	}
+
+	@GetMapping(value = "/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:/booklist";
 	}
 }
